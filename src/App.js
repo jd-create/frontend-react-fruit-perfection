762604dadfd45b7d79d3bcd79b4662ad581b27e9
jd-create <jenniferdemeijer@t-online.de>
2021-05-16 21:52:38 +0200

    event listener in button, fragment voor header and main, svg for shoppingcart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
+import Product from './Product';
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
 import './App.css';
 
+
 function App() {
-  return (
-   <main>
-     <article className={"product"}>
-       <img
-           src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
-           alt="Citroenen"
-       />
-       <h2 className={"product-name"}>Citroen</h2>
-       <p className={"product-description"}>Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
-       </p>
-     </article>
-   </main>
-  );
+    return (
+        <Fragment>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Shop</a>
+                        </li>
+                        <li>
+                            <a href="/">Ons verhaal</a>
+                        </li>
+                        <li>
+                            <a href="/">Blog</a>
+                        </li>
+                    </ul>
+                    <ShoppingCart className="shopping-cart-icon" />
+                </nav>
+                <h1>Fruit perfection</h1>
+                <button type="button"
+                onClick={() => console.log("Jij wil shoppen!")
+                }
+                >
+                    Shop nu
+                </button>
+            </header>
+        <main>
+            <Product
+                image={citroenen}
+                title="Citroen"
+                description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+            />
+            <Product
+                image={limoenen}
+                title="Limoen"
+                description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+            />
+            <Product
+                image={ijsblokjes}
+                title="IJsblokjes"
+                description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+            />
+        </main>
+        </Fragment>
+    );
 }
 
 export default App;
